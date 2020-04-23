f9b810cf0944f7d16a1dfc298549123beffdcba6
richasdy <donni.richasdy@gmail.com>
2020-04-23 09:15:05 +0700

    refactor postgresql pipeline readability

--- tutorial/pipelines/PostgreSQLInsertPipeline.py
+++ tutorial/pipelines/PostgreSQLInsertPipeline.py
@@ -12,33 +12,60 @@ logger = logging.getLogger('tutorial.pipelines.PostgreSQLInsertPipeline')
 
 class PostgreSQLInsertPipeline(object):
     def __init__(self):
-        self.conn = psycopg2.connect(
+
+        self.connection = psycopg2.connect(
             host='localhost', 
             database='scrapy_tutorial', 
             user='root', 
             password='toor'
             )
-        self.cursor = self.conn.cursor()
-        
-        '''
-        CREATE TABLE quote ( 
-            id SERIAL, 
-            author_bio TEXT, 
-            author_birthday DATE, 
-            author_bornlocation TEXT, 
-            author_name VARCHAR(50), 
-            quote_content TEXT, 
-            PRIMARY KEY (id) 
-            );
-        '''
 
-    def process_item(self, item, spider):    
+        self.cursor = self.connection.cursor()
+        
         try:
-            logger.debug('try insert data')
-            sql = "INSERT INTO quote(author_bio, author_birthday, author_bornlocation, author_name, quote_content) VALUES (%s,%s,%s,%s,%s)"
-            data = (item["author_bio"],item["author_birthday"],item["author_bornlocation"],item["author_name"],item["quote_content"])
-            self.cursor.execute(sql,data)            
-            self.conn.commit()            
+
+            ddl = "\
+            CREATE TABLE IF NOT EXISTS quote ( \
+                id SERIAL, \
+                author_bio TEXT, \
+                author_birthday DATE,\
+                author_bornlocation TEXT, \
+                author_name VARCHAR(50), \
+                quote_content TEXT, \
+                PRIMARY KEY (id) \
+            );\
+            "
+            self.cursor.execute(ddl)
+            logger.debug("CREATE TABLE IF NOT EXISTS quote")
+
         except psycopg2.DatabaseError as e:
-            print ("Error %s",(e))
+            logger.error(e)
+
+        
+
+    def process_item(self, item, spider):  
+
+        try:
+
+            logger.debug('INSERT INTO quote')
+
+            sql = "\
+                INSERT INTO quote \
+                (author_bio, author_birthday, author_bornlocation, author_name, quote_content) VALUES \
+                (%s,%s,%s,%s,%s)\
+                "
+            data = (
+                item["author_bio"],
+                item["author_birthday"],
+                item["author_bornlocation"],
+                item["author_name"],
+                item["quote_content"]
+                )
+
+            self.cursor.execute(sql,data)            
+            self.connection.commit()  
+
+        except psycopg2.DatabaseError as e:
+            logger.error(e)
+
         return item
