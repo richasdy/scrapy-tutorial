2747fad6cbc04826feb28726bd9cc341d31af783
richasdy <donni.richasdy@gmail.com>
2020-04-25 14:04:27 +0700

    refactor file

--- tutorial/items/FileItem.py
+++ tutorial/items/FileItem.py
@@ -7,26 +7,6 @@
 
 from scrapy.item import Item, Field
 from scrapy.loader.processors import MapCompose, TakeFirst
-from datetime import datetime
-
-
-def remove_quotes(text):
-    # strip the unicode quotes
-    text = text.strip(u'\u201c'u'\u201d')
-    return text
-
-
-def convert_date(text):
-    # convert string March 14, 1879 to Python date
-    return datetime.strptime(text, '%B %d, %Y')
-
-
-def parse_location(text):
-    # parse location "in Ulm, Germany"
-    # this simply remove "in ", you can further parse city, state, country, etc.
-    return text[3:]
-
-
 class FileItem(Item):
     header = Field(
         input_processor=MapCompose(str.strip),
